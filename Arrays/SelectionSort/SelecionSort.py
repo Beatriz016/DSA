28f2b6fdf412004bf8f507009ee771c8d468577c
unknown <trizvika@gmail.com>
2025-08-20 18:02:46 -0300

    Add: selection Sort Study

---
+++ Arrays/SelectionSort/SelecionSort.py
@@ -0,0 +1,12 @@
+# How it works:
+
+# Go through the array to find the lowest value.
+# Move the lowest value to the front of the unsorted part of the array.
+# Go through the array again as many times as there are values in the array.
+
+# [ 7, 12, 9, 11, 3]
+# [ 3, 7, 12, 9, 11]
+# [ 3, 7, 9, 12, 11]
+# [ 3, 7, 9, 11, 12]
+
+# Selection Sort Implementation
