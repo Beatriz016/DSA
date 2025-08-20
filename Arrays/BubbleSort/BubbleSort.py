5463960878a0fa574c310d7aa9038d074c06a43e
unknown <trizvika@gmail.com>
2025-08-20 18:09:31 -0300

    Add: bubble sort manual run through

--- Arrays/BubbleSort/BubbleSort.py
+++ Arrays/BubbleSort/BubbleSort.py
@@ -5,6 +5,16 @@
 # If the value is higher than the next one, swap the values so that the highest value comes last.
 # Go through the array as many times as there are values in the array.
 
+# [7, 12, 9, 11, 3]
+# [7, 9, 12, 11, 3]
+# [7, 9, 11, 12, 3]
+# [7, 9, 11, 3, 12]
+
+# [7, 9, 3, 11, 12]
+# [7, 3, 9, 11, 12]
+# [3, 7, 9, 11, 12]
+
+
 # Bubble Sort Implementation
 # To implement the Bubble Sort algorithm in a programming language, we need:
 
